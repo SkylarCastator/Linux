9d08053d35e945631f0c3e6ef306962241444fa0
ghost <aerialchemist@gmail.com>
2019-10-09 16:10:27 -0600

    Cleaning up setup files for linux install

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
+
+echo "Welcome, to start what do you want to set the username and password to?"
+read -p "Username : " computerUsername
+read -p "Password : " computerPassword
+useradd -m $computerUserName
+passwd $computerPassword
+
+#! Add Other Users
+#!usermod -a -G sudo user_std
+while true; do 
+  read -p "Do you wish to add more users to this device?" yn
+  case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) exit;;
+    * )echo "Please answer yes or no.";;
+  esac
+done
+
 sudo apt-get update
-apt-get dist-upgrade
-
-#!Get Vim
-sudo apt-get install vim
-
-#!Set up Vundle and copy vimrc file into file path
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#!cp ./vimrc ~/.vim/vimrc
-#!ln ~/.vimrc ~/.vim/vimrc
-
-#!Add in vim plugins 
-vim +PluginInstall +qall
+sudo apt-get dist-upgrade
 
 sudo apt-get install gnome-tweak-tool
 
-#! Need to link into git repos and download other tools
-echo "dont forget to install / update YoucompleteMe"
-echo "https://github.com/j1z0/dotfiles.git"
 
-#!Extra commmands
-apt install irssi #!Install IRC command line tool
+#!-------------------------------------------
+echo "Installing Vim"
+sh ./InstallScripts/vim.sh
 
+#!-------------------------------------------
 #!Install Git 
-#!sudo apt-get install git
-#!git config --global user.name "SkylarCastator"
-#!git config --global user.email "aerialchemist@gmail.com"
+sudo apt-get install git
+echo "Username : "
+read gitUserName
+git config --global user.name $gitUserName
+echo "Email : "
+read gitEmailAddress
+git config --global user.email $gitEmailAddress
 
+#!-------------------------------------------
 #!Download Python Tools 
 python --version
 echo "Python 2.7.15 preferred"
 apt-get install python-setuptools python-pip
 pip install github3.py
+
+#Install Python 3.7
+sudo apt-get install build-essential checkinstall
+
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+cd /usr/src
+
+sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+
+sudo tar xzf Python-3.7.4.tgz
+
+cd Python-3.7.4
+sudo ./configure --enable-optimizations
+sudo make altinstall
 
 #!Download Machine Learning and Data Analytics Python Packages
 pip install numpy
@@ -58,6 +85,8 @@ echo "nltk.download()"
 echo "select all packages"
 echo "exit()"
 
+
+#!-------------------------------------------
 apt-get install tor
 #!systemctl enable tor
 #!proxychains iceweasel
@@ -65,13 +94,12 @@ apt-get install tor
 apt-get install virtualbox
 apt-get install tilix
 apt-get install filezilla filezilla-common -y
+#!Extra commmands
+apt install irssi #!Install IRC command line tool
+
 
 #!git clone https://github.com/hak5darren/USB-Rubber-Ducky
 #!cd USB-Rubber-Ducky/Encoder/
 #!java -jar encoder.jar -i input_payload.txt -o inject.bin
 
-#!passwd root
 
-useradd -m user_std
-passwd user_std
-usermod -a -G sudo user_std
